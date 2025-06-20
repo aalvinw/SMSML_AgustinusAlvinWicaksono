7c13a9808499a6a000fcd47d2e2eeb1122462cbb
aalvinw <33995438+aalvinw@users.noreply.github.com>
2025-06-20 11:09:39 +0700

    Update modelling.py

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -1,12 +1,16 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+import numpy as np
 import mlflow
 import mlflow.sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+
+# Aktifkan autolog MLflow sekali di awal
+mlflow.autolog()
 
 # 1. Load data preprocessed
-df = pd.read_csv("preprocessed_data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/aalvinw/SMSML_AgustinusAlvinWicaksono/refs/heads/main/Workflow-CI/MLProject/preprocessed_data.csv")
 
 # 2. Pisahkan fitur dan target
 X = df.drop(columns=["Reached.on.Time_Y.N"])
@@ -17,12 +21,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# 4. Mulai tracking MLflow
+# 4. Mulai tracking MLflow dengan nested=True
 mlflow.set_experiment("Basic_Model_Logistics")
 
-with mlflow.start_run():
-    mlflow.autolog()
-
+with mlflow.start_run(nested=True):
     # 5. Model sederhana
     model = LogisticRegression(max_iter=500)
     model.fit(X_train, y_train)
